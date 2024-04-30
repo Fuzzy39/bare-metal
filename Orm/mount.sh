9cfb47896c547009e93adb3351eb243b25f4c90a
Fuzzy39 <jmcraft126@gmail.com>
2024-04-30 01:01:58 -0400

    made decent mount/unmount scripts

--- Orm/mount.sh
+++ Orm/mount.sh
@@ -1,4 +1,32 @@
-LOOP=$(losetup -f)
-echo "Mounting to: ${LOOP}"
-sudo losetup -Pf drive.iso
-sudo mount -o uid=$UID "${LOOP}p1" mnt
+#!/bin/bash
+set -u
+
+if mountpoint -q mnt; then
+    echo "Already Mounted."
+    exit 1
+fi
+
+
+if LOOP=$(losetup -f); then
+    echo "Mounting to: ${LOOP}"
+else
+    exit 1
+fi
+
+if sudo losetup -Pf drive.iso; then
+    echo "loop device created."
+else
+    echo "failed to create loop device."
+    exit 1
+fi
+
+
+if sudo mount -o uid=$UID "${LOOP}p1" mnt; then
+    echo "sucessfully mounted."
+    echo $LOOP >> openLoops.txt
+else
+    echo "failed to mount."
+    losetup -d $LOOP
+    exit 1
+fi
+
--- Orm/unmount.sh
+++ Orm/unmount.sh
@@ -1,3 +1,15 @@
-sudo umount mnt
-sudo losetup -d $1
+#!/bin/bash
+set -u
+
+
+if mountpoint -q mnt; then
+    sudo umount mnt
+fi
+
+while read p; do
+    echo "closing ${p}"
+    sudo losetup -d $p
+done <openLoops.txt
+
+> openLoops.txt
 
